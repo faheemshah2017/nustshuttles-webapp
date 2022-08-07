dc4a41674dae87162a49604c12626d13ccc0c236
Faheem Shah <faheemshahiiui@gmail.com>
2022-08-07 18:48:38 +0500

    idle time added and stoped recording data if speed is zero and time is greater than 8-5

--- routes/shuttles.js
+++ routes/shuttles.js
@@ -81,33 +81,51 @@ router.post('/sendLocation', (req, res, next) => {
     },
     datetime:new Date(parseInt(req.body.time)*1000)
   }  
-  console.log(shuttleData)
-
-  data_model.add(col_shuttlesData, shuttleData, (resp) => {
-    data_model.getDataBy(col_shuttles,"deviceId", req.body.deviceId, async (shuttle) => {
-      let data = {
-        deviceId:req.body.deviceId,
-        busNumber:shuttle.shuttleNumber,
-        latitude:req.body.latitude,
-        logitude:req.body.longitude,
-        speed:req.body.speed,
-        time:req.body.time
-      }
-      if(shuttle){
-        try{
-          const child = ref.child(req.body.deviceId)
-          await child.update(data);
+  const dataDate = new Date(shuttleData.time * 1000);
+  let hours = dataDate.getHours()+5;
+  let minutes = dataDate.getMinutes();
+  time = hours+(minutes/100)
+  console.log("data received from deviceId: "+req.body.deviceId)
+  if(time>17.00||time<8.30){
+    console.log("its off time")
+    shuttleData.idleTIme = 0;
+  }
+  else{
+    if(shuttleData.speed==0){
+      console.log("Shuttle is idle")
+      shuttleData.idleTIme = 10;
+    }
+    else{
+      console.log("shuttle is active")
+      shuttleData.idleTIme = 0;
+    }  
+    console.log("saving data for deviceId: "+req.body.deviceId)
+    data_model.add(col_shuttlesData, shuttleData, (resp) => {
+      data_model.getDataBy(col_shuttles,"deviceId", req.body.deviceId, async (shuttle) => {
+        let data = {
+          deviceId:req.body.deviceId,
+          busNumber:shuttle.shuttleNumber,
+          latitude:req.body.latitude,
+          logitude:req.body.longitude,
+          speed:req.body.speed,
+          time:req.body.time
         }
-        catch(e){
-          console.log(e)
+        if(shuttle){
+          try{
+            const child = ref.child(req.body.deviceId)
+            await child.update(data);
+          }
+          catch(e){
+            console.log(e)
+          }
         }
-      }
-      delete req.body._id;
-      data_model.updateBy(col_tracking, "deviceId", req.body.deviceId, req.body, (resp) => {
-        res.send(resp)
+        delete req.body._id;
+        data_model.updateBy(col_tracking, "deviceId", req.body.deviceId, req.body, (resp) => {
+          res.send(resp)
+        })
       })
     })
-  })
+  }
 })
 
 module.exports = router;
