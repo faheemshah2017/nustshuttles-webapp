a41fd4a761a9664d0cd4e09222bcbdfd1c19c1a4
Faheem Shah <faheemshahiiui@gmail.com>
2022-09-10 10:33:32 +0500

    route violation alert started

--- routes/shuttles.js
+++ routes/shuttles.js
@@ -164,7 +164,7 @@ function updateTracking(req,res,shuttleData,time){
     console.log(shuttle.route)
     
     let selectedRoute = shuttle.route.find(r=>{
-      typeof(r.from)
+      console.log(typeof(r.from))
       r.from = parseFloat(r.from.toString().replaceAll(":","."))
       r.to = parseFloat(r.to.toString().replaceAll(":","."))
       console.log(r.from,time)
